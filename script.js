67d630592002572ba866df27aaebac00145d57bf
LALITHOLAY <130987029+LALITHOLAY@users.noreply.github.com>
2023-07-27 16:42:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function volume_sphere() {
     //Write your code here
+	const radius = document.getElementById('radius').value;
+
+      // Calculate the volume of the sphere
+      const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+
+      // Update the volume input field with the result
+      document.getElementById('volume').value = volume.toFixed(2);
+
+      // Prevent form submission (since we handle the calculation here)
+      return false;
+    }
   
-} 
+ 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
